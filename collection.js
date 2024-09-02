1d3b8318cdf59abea899fa98a29d690587b22025
Shubhujadhav <162121811+Shubhujadhav@users.noreply.github.com>
2024-09-02 21:35:45 +0530

    Create collection.js

---
+++ collection.js
@@ -0,0 +1,18 @@
+// Collection :- storing multiple values in one variable
+// -- objects | key values
+
+let obj = {
+  //* Make a small container in memory
+  //* Key Value pairs | comma separated
+  name: "Aditya",
+  age: 18,
+  developer: true,
+};
+
+// -- array | serialized values | Index - value pairs
+
+let arr = ["a", "b", "c"]; // 0 = 'a' , 1 = 'b' , 2 = 'c'
+
+// Method = object ke andar ke functions
+// Property = values| string , boolean , number , collections
+// datatype | functions
