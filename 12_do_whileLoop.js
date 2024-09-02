5ec9c4fd91885f3326d3862f1fd139cefd00aa61
Shubhujadhav <162121811+Shubhujadhav@users.noreply.github.com>
2024-09-02 21:45:54 +0530

    Create 12_do_whileLoop.js

---
+++ 12_do_whileLoop.js
@@ -0,0 +1,9 @@
+// -- DO While Loop
+// * jabtak condition false nahi hoti tabtak repeat karna | block of code
+let i = 10;
+
+do {
+  // Block of code
+  console.log(i);
+} while (i < 10);
+// loop khatam hone se baad condition check karta hai
