ab7fa6e11eb028433865595a2e8a689db4daaf3e
Shubhujadhav <162121811+Shubhujadhav@users.noreply.github.com>
2024-09-02 21:50:37 +0530

    Create 17_String.js

---
+++ 17_String.js
@@ -0,0 +1,12 @@
+// String = data type => "Hi"
+// Double quote method
+let userName = "Rahul"
+// Single quote method
+let str = 'Hi '
+let str2 = " " + userName // " Aditya"
+console.log(str+str2) // 'Hi Aditya'
+console.log(str +" My name is "+ userName + " My age is asoshjdfnhjsd")
+
+// String Literals | Chapstick Method
+let message = `My name is ${userName}. My age is ${18} ` // ${} = placeholder means which replaces the value
+console.log(message);
