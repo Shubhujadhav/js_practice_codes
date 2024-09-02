e0f06970ba893de7ba3dbd57203c5ac581819da7
Shubhujadhav <162121811+Shubhujadhav@users.noreply.github.com>
2024-09-02 21:45:11 +0530

    Create 11_while_loops.js

---
+++ 11_while_loops.js
@@ -0,0 +1,19 @@
+// -- While Loop
+// * jabtak condition false nahi hoti tabtak repeat karna | block of code
+
+//? while (condition) {
+//?    Block of code
+//? }
+
+let i = 1; // Intialization | Global Scope
+
+// Scope
+while (i < 10) {
+  // Condition
+  // loop chalu hone se phele condition check karta hai
+  // Range = [1,3,5,.....,9]
+  console.log(i);
+  i++; // i = i+1 | Increment | Action after the loop
+}
+
+console.log("The Value of I is:", i);
