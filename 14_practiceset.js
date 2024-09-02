341259b8da9111ab5554ec44ed2a9de6a585fa59
Shubhujadhav <162121811+Shubhujadhav@users.noreply.github.com>
2024-09-02 21:47:56 +0530

    Create 14_practiceset.js

---
+++ 14_practiceset.js
@@ -0,0 +1,7 @@
+//? Sum of all elements in array using for of loop
+let arr = [90, 74, 86, 53, 29, 20, 19, 72, 91, 68, 94, 1, 19, 27, 88];
+let sum = 0;
+for (let i of arr) {
+  sum += i;
+}
+console.log(sum);
