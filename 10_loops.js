9ee4b6436fffb7f36d93385f79fe77bca0a83f3b
Shubhujadhav <162121811+Shubhujadhav@users.noreply.github.com>
2024-09-02 21:44:16 +0530

    Create 10_loops.js

---
+++ 10_loops.js
@@ -0,0 +1,14 @@
+// loops | ek hi kaam ko repeat me karna
+let i = 1;
+// While loops
+while (i <= i) {
+  // i < 10 | i is less than 10 , i <= 10 | i is less than or equal to 10
+  // 10
+  // Range of i = [10,9,8,7,6,5,4,3,2,1]
+  // true | condition
+  console.log("Hello", i); // Repeat me karna
+  // 1, 2 ,3, 4,5,6,7,8,9,10
+  i++; // i +=1 | i = i +1
+}
+
+//-- 3 Types of loop = while , do while , for loop
