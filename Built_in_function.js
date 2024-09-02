bc9e8b70655a2e13df0dc48dc0a3e000bcd63da6
Shubhujadhav <162121811+Shubhujadhav@users.noreply.github.com>
2024-09-02 21:37:16 +0530

    Create Built_in_function.js

---
+++ Built_in_function.js
@@ -0,0 +1,18 @@
+// variable definition = variable ki value
+// function definition = function kya karega
+
+// Built in functions | the browser provided functions
+
+//? Console.log
+let a = "Hello";
+console.log("a"); // Prints the value provided in the brackets | or as arguments
+console.log(a); // Prints the value provided in the brackets | or as arguments
+
+//? Alert
+alert("Hi"); // Alerts through a model in browser | Browser provided function
+
+//? Prompt
+prompt("Enter your age."); // Shows the message and asks for user input | Browser provided function
+(input) => string;
+//? Confirm
+confirm("Enter your age."); // Shows the message and asks for user input in true or false | Browser provided function
